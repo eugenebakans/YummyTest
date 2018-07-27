64e0c35bcc929dda18f08f1510c50d23801eeb13
Eugene <eugene.bakans@gmail.com>
2018-07-27 09:24:13 +0300

    ProfileMenuBlock update

--- src/test/java/com/bakans/yummytest/elements/ProfileMenuBlock.java
+++ src/test/java/com/bakans/yummytest/elements/ProfileMenuBlock.java
@@ -1,6 +1,21 @@
 package com.bakans.yummytest.elements;
 
+import com.bakans.yummytest.pages.ProfileSettingsPage;
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
+import ru.yandex.qatools.htmlelements.element.Link;
 
+import java.util.List;
+
+@Name("Profile Menu")
+@FindBy(xpath = ".//div[@class = 'content full-width']/div[@class = 'clearfix row']/div[2]")
 public class ProfileMenuBlock extends HtmlElement {
+
+    @FindBy(xpath = "./div[@class = 'inner-block']/ul/li")
+    private List<Link> menuLinks;
+
+    public void openSettingsPage() {
+        menuLinks.get(4).click();
+    }
 }
